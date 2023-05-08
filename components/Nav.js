4026242b91f2882aef8857bf6c672a5bea175f8e
jrodrz <rekoshrodriguez81@gmail.com>
2023-05-07 22:29:16 -0500

    changed nav.js and style.css

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,16 @@
 import html from "html-literal";
 
 export default links => html`
- <nav>
-  <i class="fas fa-bars"></i>
-  <ul>
-    <li>Home</li>
-    <li>STLMap</li>
-    <li>Gym Maps</li>
-    <li>Doggy Day Cares</li>
-    <li>About Us</li>
-  </ul>
-</nav>
+  <nav>
+    <i class="fas fa-bars"></i>
+    <ul class="hidden--mobile nav-links">
+      ${links
+        .map(
+          link =>
+            `<li><a href="/${link.title}" title="${link.title}" data-navigo>${link.text}</a></li>`
+        )
+        .join("")}
+    </ul>
+  </nav>
 `;
 
