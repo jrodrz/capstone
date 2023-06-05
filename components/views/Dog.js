a0017c933fb48d04ee9b1893aca57e1cce4498a6
jrodrz <rekoshrodriguez81@gmail.com>
2023-06-05 16:33:52 -0500

    went old school on the day cares

--- components/views/Dog.js
+++ components/views/Dog.js
@@ -3,19 +3,14 @@ import html from "html-literal";
 
 export default state => html`
 <p>
-  <h4> Day care places for my</h4>
+  <h2> Day cares for my furbaby.
 </p>
-
-<table id="dogs">
-<tr>
-  <th>Dog</th>
- </tr>
-${state.dogs
-  .map(dog => {
-    return `<tr><td>${dog.dog}</td></tr>`;
-  })
-  .join("")}
-
-
-</table>
-`;
+<p>
+  <h3>Wolfgang's Pet Stop, 330 N Euclid Ave, St. Louis, MO 63108, wolfgangspetstop.com
+</p>
+<p>
+  <h3>The Watering Bowl, 5215 Manchester Ave., St. Louis, MO 63110, www.thewateringbowl.com
+</p>
+<p>
+  <h3>Saint Louis Dog Butler, 4818 Washington Ave., St. Louis, MO 63108, www.saintlouisdogbutler.com
+</p>`
